085d545e722261a9545c18ed4e8365157a8b0637
polluterofminds <justin.edward.hunter@gmail.com>
2024-01-30 08:18:40 -0600

    fixed username and pfp issue

--- src/utils/fc.ts
+++ src/utils/fc.ts
@@ -2,8 +2,10 @@ export const getUserByFid = async (fid: number) => {
   const res = await fetch(`https://nemes.farcaster.xyz:2281/v1/userDataByFid?fid=${fid}`)
   const json = await res.json();
   const { messages } = json;
-  const username = messages[0].data.userDataBody.value;
-  const pfp = messages[1].data.userDataBody.value;
+  const usernamePayload = messages.find((m: any) => m.data.userDataBody.type === "USER_DATA_TYPE_USERNAME")
+  const username = usernamePayload.data.userDataBody.value;
+  const pfpPayload = messages.find((m: any) => m.data.userDataBody.type === "USER_DATA_TYPE_PFP")
+  const pfp = pfpPayload.data.userDataBody.value;
   return {
     fid: fid, 
     username, 
