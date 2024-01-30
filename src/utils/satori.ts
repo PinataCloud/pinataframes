6d1cae1dad2044b134bbf783043505659fdabe59
Steve <stevedsimkins@gmail.com>
2024-01-29 22:54:01 -0500

    another image size test

--- src/utils/satori.ts
+++ src/utils/satori.ts
@@ -16,8 +16,8 @@ export const satoriHelper = async (requestBody: any) => {
   );
 
   const ogOptions: SatoriOptions = {
-    width: 1200,
-    height: 628.8,
+    width: 800,
+    height: 418,
     // debug: true,
     embedFont: true,
     fonts: [
