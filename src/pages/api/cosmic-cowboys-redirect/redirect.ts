231326e8b62952a6bd55dddf1806729309209534
paigexx <pcjones12@gmail.com>
2024-02-01 15:20:10 -0600

    added redirect client side

--- src/pages/api/cosmic-cowboys-redirect/redirect.ts
+++ src/pages/api/cosmic-cowboys-redirect/redirect.ts
@@ -1,41 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { generateImage } from "@/utils/satori";
-import { getLastFourMessages } from "@/utils/storage";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-  maxDuration: 300,
-};
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method === "GET") {
-    try {
-        const redirectUrl = 'https://www.cosmiccowboys.cloud/';
-        // Set the Location header for redirection
-        res.setHeader('Location', redirectUrl);
-        // Set the status code for redirection (302 is the default for temporary redirection)
-        res.status(302);
-        // End the response to trigger the redirection
-        res.end();
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Server error");
-    }
-  } else if (req.method === "POST") {
-    try {
-        const redirectUrl = 'https://www.cosmiccowboys.cloud/';
-        // Set the Location header for redirection
-        res.setHeader('Location', redirectUrl);
-        // Set the status code for redirection (302 is the default for temporary redirection)
-        res.status(302);
-        // End the response to trigger the redirection
-        res.end();
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Server error");
-    }
-  }
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  return NextResponse.redirect(`${process.env.HOSTED_URL}/redirect`, {status: 302});
 }
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
