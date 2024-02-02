e9b3c804dfe6f438e35784735898f5210ba5d5e9
Steve <stevedsimkins@gmail.com>
2024-02-02 14:58:07 -0500

    test

--- src/pages/api/cosmic-cowboys-redirect/redirect.ts
+++ src/pages/api/cosmic-cowboys-redirect/redirect.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect(`${process.env.HOSTED_URL}/redirect`, {status: 302});
+  return NextResponse.redirect(`https://www.pinatadrops.com/redirect`, {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
