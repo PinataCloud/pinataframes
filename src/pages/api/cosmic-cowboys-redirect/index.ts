952aa9aac1c34050209877c2aa97d56fe6df3d63
paigexx <pcjones12@gmail.com>
2024-02-02 14:37:53 -0600

    updates file name

--- src/pages/api/cosmic-cowboys-redirect/index.ts
+++ src/pages/api/cosmic-cowboys-redirect/index.ts
@@ -26,7 +26,7 @@ export default async function handler(
           <meta property="fc:frame:image" content="${imgUrl}" />
           <meta property="fc:frame:button:1" content="Play Beta" />
           <meta property="fc:frame:button:1:action" content="post_redirect" />
-          <meta property="fc:frame:post_url "content="https://t3-emoji-livid.vercel.app/api/cosmic-cowboys-redirect/redirect" />
+          <meta property="fc:frame:post_url "content="${process.env.HOSTED_URL}/api/cosmic-cowboys-redirect/link" />
         <title>Cosmic Cowboys</title>
         </head>
         <body>
