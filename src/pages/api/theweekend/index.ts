0f3120003f18c510924c3036fe1dbf727a95997b
Steve <stevedsimkins@gmail.com>
2024-02-02 20:44:32 -0500

    added the weekend

---
+++ src/pages/api/theweekend/index.ts
@@ -0,0 +1,104 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+import { mintFrame } from "@/utils/tfocMint";
+import { getConnectedAddressForUser } from "@/utils/fc";
+
+const HUB_URL = process.env["HUB_URL"] || "hub.pinata.cloud";
+const client = getSSLHubRpcClient(HUB_URL);
+
+export const config = {
+  maxDuration: 300,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    try {
+      //  Template should have a post_url that matches the index of the plane selected
+      const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <meta property="fc:frame:image" content="https://dweb.mypinata.cloud/ipfs/QmTidtsgh4faygkV3Fj1f2gfdLNYCFh3gsgNVWRsGwSQbA" />
+          <meta property="fc:frame:button:1" content="Is it the weekend?" />
+          <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/theweekend" />
+          <meta property="fc:frame" content="vNext" />          
+       <title>The Weekend</title>
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.cloud/ipfs/QmTidtsgh4faygkV3Fj1f2gfdLNYCFh3gsgNVWRsGwSQbA" />
+        </body>
+      </html>`;
+      res.send(template1);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server error");
+    }
+  } else if (req.method === "POST") {
+    try {
+      var date = new Date();
+      date.setHours(date.getHours() - date.getTimezoneOffset() / 60 + 5);
+      var dayOfWeek = date.getDay();
+      console.log(dayOfWeek)
+      var hours = date.getHours();
+      console.log(hours)
+      // Check if it's the weekend or not
+      var isWeekend =
+        dayOfWeek === 0 ||
+        dayOfWeek === 6 ||
+        (dayOfWeek === 5 && hours >= 17) ||
+        (dayOfWeek === 1 && hours < 24);
+      if (!isWeekend) {
+        //  Template should have a post_url that matches the index of the plane selected
+        const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <title>The Weekend</title>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame:image" content="https://dweb.mypinata.cloud/ipfs/QmQgqbUnCVwcXvdXjPMxEvY2vTv9HLY3ZwjG6rQS9Jc8XN" />
+          <meta property="fc:frame:button:1" content="Check again" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/theweekend" />
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.cloud/ipfs/QmQgqbUnCVwcXvdXjPMxEvY2vTv9HLY3ZwjG6rQS9Jc8XN" />
+        </body>
+      </html>`;
+        return res.send(template1);
+        // } else {
+        //   return res.status(401).send("Unauthorized");
+        // }
+      } else {
+        const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <title>It is the weekend :)</title>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame:image" content="https://dweb.mypinata.cloud/ipfs/QmeDejkafV9hwBE2zYmmfUkLb5AjqPFpRUY2XkbLd8iq6n" />
+          <meta property="fc:frame:button:1" content="Mint the Weekend" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/theweekend/mint" />
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.cloud/ipfs/QmeDejkafV9hwBE2zYmmfUkLb5AjqPFpRUY2XkbLd8iq6n" />
+        </body>
+      </html>`;
+        return res.send(template1);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server error");
+    }
+  }
+}
---
+++ src/pages/api/theweekend/mint.ts
@@ -0,0 +1,102 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+import { mintFrame } from "@/utils/tfocMint";
+import { getConnectedAddressForUser } from "@/utils/fc";
+
+const HUB_URL = process.env["hub-grpc.pinata.cloud"] || "hub.pinata.cloud";
+const client = getSSLHubRpcClient(HUB_URL);
+
+export const config = {
+  maxDuration: 300,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  var date = new Date();
+  date.setHours(date.getHours() - date.getTimezoneOffset() / 60 + 5);
+  var dayOfWeek = date.getDay();
+  console.log(dayOfWeek);
+  var hours = date.getHours();
+  console.log(hours);
+  // Check if it's the weekend or not
+  var isWeekend =
+    dayOfWeek === 0 ||
+    dayOfWeek === 6 ||
+    (dayOfWeek === 5 && hours >= 17) ||
+    (dayOfWeek === 1 && hours < 24);
+  try {
+    console.log(req.body);
+    if (req.body.untrustedData.buttonIndex === 1) {
+      //  Template should have a post_url that matches the index of the plane selected
+      console.log("MINTING");
+      //  Verify the signature from the payload
+      const frameMessage = Message.decode(
+        Buffer.from(req.body?.trustedData?.messageBytes || "", "hex"),
+      );
+      const result = await client.validateMessage(frameMessage);
+      // if (result.isOk() && result.value.valid) {
+      //  If verified, randomly select a plane to display
+      if (/* result.isOk() && result.value.valid && */ isWeekend) {
+        const connectedAddressData = await getConnectedAddressForUser(
+          req.body.untrustedData.fid,
+        );
+        if (!connectedAddressData || connectedAddressData.length === 0) {
+          console.log("No connected address");
+          return res.status(400).send("No connected address");
+        }
+
+        const connectedAddress = connectedAddressData[0].connectedAddress;
+        //  Mint the plane to the wallet
+        const tx = await mintFrame(connectedAddress, "ipfs://METADATA_CID");
+        console.log({ tx });
+        //  Template should have a post_url that matches the index of the plane selected
+        const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame:image" content="https://dweb.mypinata.cloud/ipfs/QmZpFMrCFc8Xs3orLBUukiG3f1VfNaHweSZYTB2SyZGyfc" />
+          <meta property="fc:frame:button:1" content="Check Again" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/theweekend" />
+        <title>The Weekend</title>
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.cloud/ipfs/QmZpFMrCFc8Xs3orLBUukiG3f1VfNaHweSZYTB2SyZGyfc" />
+        </body>
+      </html>`;
+        return res.send(template1);
+      } else {
+        console.log("already minted or not the weekend")
+        const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame:image" content="https://dweb.mypinata.cloud/ipfs/QmRfY43eP6ySdEPE94ZQ2fDve3VQEpdbmYAgj4jdCgDsjR" />
+          <meta property="fc:frame:button:1" content="Check Again" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/theweekend" />
+        <title>The Weekend</title>
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.cloud/ipfs/QmRfY43eP6ySdEPE94ZQ2fDve3VQEpdbmYAgj4jdCgDsjR" />
+        </body>
+      </html>`;
+        return res.send(template1);
+      }
+      // } else {
+      //   return res.status(401).send("Unauthorized");
+      // }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+}
