0f06a8837649bf7a0591e34f0df185fd71c36348
Steve <stevedsimkins@gmail.com>
2024-02-02 21:28:22 -0500

    come on

--- src/pages/api/the-weekend/mint.ts
+++ src/pages/api/the-weekend/mint.ts
@@ -14,6 +14,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  console.log(req.body)
   var date = new Date();
   date.setHours(date.getHours() - date.getTimezoneOffset() / 60 + 5);
   var dayOfWeek = date.getDay();
