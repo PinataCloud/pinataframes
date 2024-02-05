32b08e544b2277ed80172b42a6d0b8266d215ead
Steve <stevedsimkins@gmail.com>
2024-02-05 12:13:51 -0500

    fix

--- src/pages/pocketcast/index.tsx
+++ src/pages/pocketcast/index.tsx
@@ -7,8 +7,8 @@ const frameMetadata = getFrameMetadata({
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+  image: `https://dweb.mypinata.cloud/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`,
+  post_url: `${process.env.HOSTED_URL}/api/frame?id=1`,
 });
 
 export const metadata: Metadata = {
