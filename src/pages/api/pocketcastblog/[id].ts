e35dac0bddee7d3b5b629fb77cd46d7fd5e7a58e
Steve <stevedsimkins@gmail.com>
2024-02-05 12:55:11 -0500

    tweak html

--- src/pages/api/pocketcastblog/[id].ts
+++ src/pages/api/pocketcastblog/[id].ts
@@ -14,13 +14,24 @@ export default async function handler(
       console.log(req.body);
       //  Verify the signature from the payload
       //  Template should have a post_url that matches the index of the plane selected
-      const template1 = `<!DOCTYPE html><html><head>
-          <title>This is frame ${id}</title>
-          <meta property="fc:frame" content="vNext" />
+      const template1 = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <meta property="fc:frame:image" content="https://dweb.mypinata.clou/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/${id}.png" />
-          <meta property="fc:frame:button:1" content="Next Page" />
+          <meta property="fc:frame:button:1" content="Different plane" />
+          <meta property="fc:frame:button:2" content="Mint Plane" />
+          <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:post_url" content="${process.env.HOSTED_URL}/api/pocketcastblog/${nextId}" />
-        </head></html>`;
+          <title>This is frame ${id}</title>
+        </head>
+        <body>
+          <img src="https://dweb.mypinata.clou/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/${id}.png" />
+        </body>
+      </html>`;
       return res.send(template1);
     } catch (error) {
       console.log(error);
