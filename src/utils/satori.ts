91a8622f2bbec50cc2e7ecab022db743800cf5ed
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-19 16:05:53 -0600

    added analytics tracking and image generation

--- src/utils/satori.ts
+++ src/utils/satori.ts
@@ -5,6 +5,54 @@ import { getIconCode, loadEmoji } from "./twemoji";
 import fs from 'fs'
 import path from 'path';
 
+type FrameDataByDate = {
+  date: Date,
+  frame_id:string,
+  interaction_count: number, 
+  button_index_1_interactions?: number, 
+  button_index_2_interactions?: number, 
+  button_index_3_interactions?: number,
+  button_index_4_interactions?: number
+}
+
+interface FrameAnalyticsResponse {
+    total_interactions: number,
+    total_unique_interactions: number,
+    dates: FrameDataByDate[]  
+}
+
+export const uploadToIpfs = async (image: any) => {
+  try {
+    const tempPath = path.join("/tmp", "image.png");
+    fs.writeFileSync(tempPath, image);
+    const file = fs.readFileSync(tempPath);
+    const formData = new FormData();
+    formData.append("file", new Blob([file]), "image.png");
+
+    const metadata = JSON.stringify({
+      name: `image.png`,
+    });
+    formData.append("pinataMetadata", metadata);
+    const imageUpload = await fetch(
+      "https://api.pinata.cloud/pinning/pinFileToIPFS",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.PINATA_JWT}`,
+        },
+        body: formData,
+      },
+    );
+    const { IpfsHash } = await imageUpload.json();
+
+    const url = `${process.env.GATEWAY_URL}/ipfs/${IpfsHash}?filename=image.png`;
+    console.log({ url });
+    return url;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 export const satoriHelper = async (requestBody: any) => {
   console.log(requestBody);
@@ -95,33 +143,140 @@ export const satoriHelper = async (requestBody: any) => {
 export const generateImage = async (requestData: any) => {
   try {
     const image = await satoriHelper(requestData);
-    const tempPath = path.join("/tmp", "image.png");
-    fs.writeFileSync(tempPath, image);
-    const file = fs.readFileSync(tempPath);
-    const formData = new FormData();
-    formData.append("file", new Blob([file]), "image.png");
-
-    const metadata = JSON.stringify({
-      name: `image.png`,
-    });
-    formData.append("pinataMetadata", metadata);
-    const imageUpload = await fetch(
-      "https://api.pinata.cloud/pinning/pinFileToIPFS",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.PINATA_JWT}`,
-        },
-        body: formData,
-      },
-    );
-    const { IpfsHash } = await imageUpload.json();
-
-    const url = `${process.env.GATEWAY_URL}/ipfs/${IpfsHash}?filename=image.png`;
-    console.log({ url });
+    const url = await uploadToIpfs(image);
     return url;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
+
+export const satoriAnalyticsHelper = async (data: FrameAnalyticsResponse) => {
+  const monoFontReg = await fetch(
+    "https://api.fontsource.org/v1/fonts/inter/latin-400-normal.ttf",
+  );
+
+  const monoFontBold = await fetch(
+    "https://api.fontsource.org/v1/fonts/inter/latin-700-normal.ttf",
+  );
+
+  // const ogOptions: SatoriOptions = {
+  //   width: 1200,
+  //   height: 630,
+  //   debug: true,
+  //   embedFont: true,
+  //   fonts: [
+  //     {
+  //       name: "Roboto Mono",
+  //       data: await monoFontReg.arrayBuffer(),
+  //       weight: 400,
+  //       style: "normal",
+  //     },
+  //     {
+  //       name: "Roboto Mono",
+  //       data: await monoFontBold.arrayBuffer(),
+  //       weight: 700,
+  //       style: "normal",
+  //     },
+  //   ],
+  //   // loadAdditionalAsset: async (code: any, segment: any) => {
+  //   //   if (code === "emoji") {
+  //   //     return `data:image/svg+xml;base64,${btoa(
+  //   //       await loadEmoji("twemoji", getIconCode(segment)),
+  //   //     )}`;
+  //   //   }
+  //   //   return [];
+  //   // },
+  // };
+
+  // const markup = (): any => html`
+  //   <div
+  //     tw="flex flex-col justify-center p-12 items-center text-2xl font-semibold bg-[#5F61F2] w-full h-full"
+  //   >
+  //     <h1 tw="text-2xl text-gray-900">This frame's stats</h1>
+  //     <div tw="w-full flex flex-row justify-around items-center">
+  //       <div tw="text-center flex flex-col mr-2">
+  //         <h3 tw="text-xl">Interactions</h3>
+  //         <h5 tw="text-md font-bold">${data.total_interactions}</h5>
+  //       </div>
+  //       <div tw="text-center flex flex-col">
+  //       <h3 tw="text-xl">Unique Interactions</h3>
+  //       <h5 tw="text-md font-bold">${data.total_unique_interactions}</h5>
+  //     </div>
+  //     </div>
+  //   </div>
+  // `;
+
+  // const svg = await satori(markup(), ogOptions);
+  // const png = new Resvg(svg).render().asPng();
+
+  const template: any = html(`
+<div style="padding: 20px; display: flex; flex-flow: column nowrap; justify-content: center; align-items: center; width: 1200px; height: 630px; backgroundColor: #0AAB5E; color: #fff;">
+  <div style="width: 100%; display: flex; flex-direction: column;">
+    <img style="width: 80px; margin: auto;" src="https://azure-tiny-tahr-350.mypinata.cloud/ipfs/Qmb1QqaaqBsZU7UihG93kw7GyjF6fe2zRo395z7xRojbwV" />
+  </div>
+  <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 6px; padding: 12px; color: #fff; font-size: 22px;">
+    <h1>This frame's stats</h1>
+    <div style="display: flex; flex-direction: row; align-items: center;">
+      <div style="display: flex; flex-direction: column; align-items: center; margin-right: 10px;">
+        <h3>Total Interactions</h3>
+        <h5 style="font-size: 24px;">${data.total_interactions.toLocaleString()}</h5>
+      </div>
+      <div style="display: flex; flex-direction: column; align-items: center; margin-left: 10px;">
+        <h3>Unique Interactions</h3>
+        <h5 style="font-size: 24px;">${data.total_unique_interactions.toLocaleString()}</h5>
+      </div>
+    </div>
+  </div>
+</div>
+`);
+ 
+// convert html to svg
+const svg = await satori(template, {
+  width: 1200,
+  height: 630,
+  fonts: [
+        {
+          name: "Roboto Mono",
+          data: await monoFontReg.arrayBuffer(),
+          weight: 400,
+          style: "normal",
+        },
+        {
+          name: "Roboto Mono",
+          data: await monoFontBold.arrayBuffer(),
+          weight: 700,
+          style: "normal",
+        },
+      ]
+});
+ 
+// render png
+const resvg = new Resvg(svg, {
+  background: "rgba(238, 235, 230, .9)",
+});
+const pngData = resvg.render();
+const png = pngData.asPng();
+
+  return png;
+}
+
+export const generateAnalyticsImage = async (frame_id: string) => {
+  try {
+    //  Get analytics data here
+    const res = await fetch(`https://api.pinata.cloud/farcaster/frames/interactions?frame_id${frame_id}&start_date=2023-11-21%2021:16:20&end_date=2024-02-19%2023:59:59`, {
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`, 
+
+      }
+    })
+    const json: FrameAnalyticsResponse = await res.json();
+    console.log(json);
+    const image = await satoriAnalyticsHelper(json);
+    const url = await uploadToIpfs(image);
+    return url;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
