99ec75456e306474277ac7ad0ae300e4729f55e7
Paulo Madronero <paulo@pinata.cloud>
2024-02-21 20:47:03 -0500

    test: cron job per minute

---
+++ src/pages/api/race/cron/index.ts
@@ -0,0 +1,63 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { headers } from 'next/headers'
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
+
+interface AnalyticsResponse {
+  button_index: number;
+  interaction_count: number;
+  unique_interactions: number;
+}
+
+const FRAME_ID = "pinata_race";
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const authHeader = request.headers.authorization;
+
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return response.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const first_race = dayjs('2024-02-21T10:00:00').utc();
+  const now = dayjs.utc();
+
+  const hoursDifference = now.diff(first_race, 'hour');
+
+  const hoursArray = [];
+
+  for (let i = 0; i < hoursDifference; i++) {
+    const utcStartHour = first_race.add(i, 'hour').format('YYYY-MM-DD HH:mm:ss');
+    const utcEndHour = first_race.add(i + 1, 'hour').format('YYYY-MM-DD HH:mm:ss');
+
+    console.log('utcStartHour', utcStartHour)
+    console.log('utcEndHour', utcEndHour)
+
+    const url = `${process.env.PINATA_API}/farcaster/frames/interactions/top?by=button_index&start_date=${utcStartHour}&end_date=${utcEndHour}&frame_id=${FRAME_ID}`;
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`,
+      }
+    })
+    const json: AnalyticsResponse [] = await res.json();
+    hoursArray.push(json);
+  }
+
+  // const startDate = dayjs(now).format('YYYY-MM-DD HH:mm:ss');
+  // const endDate = dayjs(today).format('YYYY-MM-DD HH:mm:ss');
+
+  // const result = await fetch(
+  //   'http://worldtimeapi.org/api/timezone/America/Chicago',
+  // );
+  // const data = await result.json();
+
+  const data = {
+    first_race,
+    now,
+    hoursDifference,
+    hoursArray
+  }
+
+  return response.json({ data });
+}
