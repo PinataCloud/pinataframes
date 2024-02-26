81fb268a73104b4a8989583ccdaac7dc6b9b7885
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-26 12:33:22 -0600

    first test of farcoder

---
+++ src/pages/api/farcoder/[id].tsx
@@ -0,0 +1,125 @@
+import { PinataFDK } from "pinata-fdk";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const fdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT!,
+  pinata_gateway: process.env.GATEWAY_URL!
+}
+);
+
+const FRAME_ID = "farcoder";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
+  if (req.method === "POST") {
+    console.log('body', req.body);
+    try {
+      const isValidated = await fdk.validateFrameMessage(req.body);
+
+      if (!isValidated) {
+        return res.status(400).json({ error: "Invalid frame message" });
+      }
+
+      await fdk.sendAnalytics(FRAME_ID, req.body);
+
+      let imageUrl = ""
+      let postEndpoint = "";
+      let buttons: any = [{ label: 'Submit answer', action: 'post' }]
+      let possibleAnswers = [""]
+      switch (req.query.id) {
+        case "2":
+          possibleAnswers = ["fcPhrases[2]", "fcPhrases[2];", "console.log(fcPhrases[2])"]
+          if (possibleAnswers.includes(req.body.untrustedData.inputText)) {
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmQRzB7mNnSBhCpQUKA9w7ar3avGKzoV9xvQV4849jvkJa`;
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/3`
+            break;
+          } else {
+            //  Game over screen
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmevCfJbagZiBhWYs1y2nLUMo3AQ7NcDwnvJkk9fswf9nN`
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          }
+        case "3":
+          possibleAnswers = ['first + second + third', 'first + second + third;', '`${first}${second}${third}`', '`${first}${second}${third}`;']
+          if (possibleAnswers.includes(req.body.untrustedData.inputText)) {
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmT42dBmiphaXAXYkTebavKUeKqKBCm6MG3ANm2mCPALLn`;
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/4`
+            break
+          } else {
+            //  Game over screen  
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmSeftd8vDvhYUb8SYRnDswzzKDarST7UxKnDkXQ9Dz7w4`
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          }
+        case "4":
+          possibleAnswers = ["console.log(getDwrFidOption3(fids))", "console.log(getDwrFidOption3(fids));"]
+          if (possibleAnswers.includes(req.body.untrustedData.inputText)) {
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmQnHSEGWQy4WUMjUJcoTcdbMYVfQCjGgPQ5pGtnrVWx1b`;
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/5`
+            break;
+          } else {
+            //  Game over screen
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmSPqx6gtVyiUM5psLVoLcoCpmkUEXibmCP6Pm2QoC36Nv`
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          }
+        case "5":
+          possibleAnswers = ['bio["TEAPOT"]', 'bio["TEAPOT"];', 'bio.TEAPOT', 'bio.TEAPOT;']
+          if (possibleAnswers.includes(req.body.untrustedData.inputText)) {
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmUeyLP9cdmcFZzZPqYLsSRtcUWXqn4QvoUL2cVPMcR7HM`;
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/6`
+            break
+          } else {
+            //  Game over screen
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmSPqx6gtVyiUM5psLVoLcoCpmkUEXibmCP6Pm2QoC36Nv`
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          }
+        case "6":
+          possibleAnswers = ["farcoder.slice(4, farcoder.length)", "farcoder.slice(4, farcoder.length);", "farcoder.slice(4)", "farcoder.slice(4);", "farcoder.slice(-14)", "farcoder.slice(-14);", "farcoder.substring(4)", "farcoder.substring(4);", "farcoder.substr(4)", "farcoder.substr(4);", 'farcoder.substring(farcoder.indexOf("I"))', 'farcoder.substring(farcoder.indexOf("I"));', 'farcoder.split("0101")[1]', 'farcoder.split("0101")[1];']
+          if (possibleAnswers.includes(req.body.untrustedData.inputText)) {
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmXo2kyujfrXZ8aakZuSfZ3ixgEocMHJsvpf2SrAK4pdmy`;
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          } else {
+            //  Game over
+            imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmaEjjAzgeztW3A3UbmfKruDgCb3oRDEPsV5N6keor9t7w`
+            postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/1`
+            buttons = [{ label: 'Play again', action: 'post' }]
+            break;
+          }
+        case "1":
+        default:
+          imageUrl = `${process.env.GATEWAY_URL}/ipfs/QmTR1N24nXEkC4K3AAsXvV9j2ABJYQdBfDpkFKXNh4rwuX`;
+          postEndpoint = `${process.env.HOSTED_URL}/api/farcoder/2`
+          break;        
+      }
+
+      const frameMetadata = await fdk.getFrameMetadata({
+        post_url: postEndpoint,
+        buttons: buttons,
+        image: { url: imageUrl, ipfs: false }
+      });
+
+
+      const frameRes =
+        `<!DOCTYPE html><html><head>
+            <title>Pinata basketball</title>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="og:title" content="Farcoder" />
+            <meta property="og:description" content="Solve coding puzzles, become a Farcoder." />
+            ${frameMetadata}
+            </head></html>`;
+
+      return res.setHeader('content-type', 'text/html').send(frameRes);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server error");
+    }
+  }
+}
+
---
+++ src/pages/apps/farcoder/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>FC Users by time</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={"https://dweb.mypinata.cloud/ipfs/QmWpwJdob9Rmnd6hKwoMiNacpwDZFiP5rCXr7GJTPCo98G?pinataGatewayToken=0MVUNvM_URS89s-TMmsBcU9EFCTGN1SYCwhUjh6VnsPahpHiVBHXBi_D_ECXYUCV"} />
+        <meta property="og:image" content={"https://dweb.mypinata.cloud/ipfs/QmWpwJdob9Rmnd6hKwoMiNacpwDZFiP5rCXr7GJTPCo98G?pinataGatewayToken=0MVUNvM_URS89s-TMmsBcU9EFCTGN1SYCwhUjh6VnsPahpHiVBHXBi_D_ECXYUCV"} />
+        <meta property="og:title" content="Farcoder" />
+        <meta property="og:description" content="Solve coding puzzles, become a Farcoder." />
+        <meta property="fc:frame:button:1" content="Start coding" />
+        <meta property="fc:frame:post_url" content={`${process.env.HOSTED_URL}/api/farcoder`} />
+      </Head>
+      <h1>Farcoder</h1>
+      <p>Solve coding problems by writing code in-frame. Climb the leaderboard.</p>
+      <p>Become a Farcoder hero.</p>
+
+      <Image src="https://solclones-dev.mypinata.cloud/ipfs/QmWpwJdob9Rmnd6hKwoMiNacpwDZFiP5rCXr7GJTPCo98G?pinataGatewayToken=0MVUNvM_URS89s-TMmsBcU9EFCTGN1SYCwhUjh6VnsPahpHiVBHXBi_D_ECXYUCV" width={400} height={200} alt="Code" />
+    </>
+  );
+}
