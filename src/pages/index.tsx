1c0fb9370d8b220450dd20e94dde5b75dcd61cd0
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-27 11:15:42 -0600

    added farcoder to resources

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,57 +13,63 @@ type Card = {
 
 const cards: Card[] = [
   {
-    title: "Pinata 500", 
-    imgUrl: "https://pamadd.mypinata.cloud/ipfs/QmeZ7Gfwj1hKpE9y7oLReQsc6NpRyZFWFC7DCxDr2TpCzX", 
-    castUrl: "https://warpcast.com/polluterofminds/0x60647c97", 
+    title: "Farcoder",
+    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmWpwJdob9Rmnd6hKwoMiNacpwDZFiP5rCXr7GJTPCo98G",
+    castUrl: "https://warpcast.com/polluterofminds/0x276b4b45",
+    githubUrl: "https://github.com/PinataCloud/pinataframes/blob/main/src/pages/api/farcoder/%5Bid%5D.tsx"
+  },
+  {
+    title: "Pinata 500",
+    imgUrl: "https://pamadd.mypinata.cloud/ipfs/QmeZ7Gfwj1hKpE9y7oLReQsc6NpRyZFWFC7DCxDr2TpCzX",
+    castUrl: "https://warpcast.com/polluterofminds/0x60647c97",
     githubUrl: "https://github.com/PinataCloud/pinataframes/blob/main/src/pages/api/race/index.ts"
   },
   {
-    title: "Analytics Frame", 
-    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmPqhuQ8HABhpFPVm3UsLoS5FJoQj8WH8yCEs9C9jxtA8K?filename=image.png", 
-    castUrl: "https://warpcast.com/polluterofminds/0x3b83b65e", 
+    title: "Analytics Frame",
+    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmPqhuQ8HABhpFPVm3UsLoS5FJoQj8WH8yCEs9C9jxtA8K?filename=image.png",
+    castUrl: "https://warpcast.com/polluterofminds/0x3b83b65e",
     githubUrl: "https://github.com/PinataCloud/pinataframes/blob/main/src/pages/api/analytics-frame/index.ts"
   },
   {
-    title: "Cosmic Cowboys", 
-    imgUrl: "https://proxy.warpcast.com/?url=https%3A%2F%2Fazure-tiny-tahr-350.mypinata.cloud%2Fipfs%2FQmdEeeA3VvYFWoB9H5XXej7EowQHx2xCvgzschSX7pdCib&s=f9817af87a5519da12213befbffcfa0baac3e89d2f15d4a0a6edf02378d269a3", 
-    castUrl: "https://warpcast.com/polluterofminds/0x1d0a7bef", 
+    title: "Cosmic Cowboys",
+    imgUrl: "https://proxy.warpcast.com/?url=https%3A%2F%2Fazure-tiny-tahr-350.mypinata.cloud%2Fipfs%2FQmdEeeA3VvYFWoB9H5XXej7EowQHx2xCvgzschSX7pdCib&s=f9817af87a5519da12213befbffcfa0baac3e89d2f15d4a0a6edf02378d269a3",
+    castUrl: "https://warpcast.com/polluterofminds/0x1d0a7bef",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/cosmic-cowboys-game"
-  }, 
+  },
   {
-    title: "Magic 8 Ball", 
-    imgUrl: "https://proxy.warpcast.com/?url=https%3A%2F%2Fdweb.mypinata.cloud%2Fipfs%2Fbafybeidelrv4tp46l4mn5yzwnzdjtb3p2vyjhqu7mei73vetwtcdkhn4ja&s=7cb5758d9d6c353399548561217d33bfc80150179f7f74c73258116738cd7d34", 
-    castUrl: "https://warpcast.com/polluterofminds/0x786f8f4e", 
+    title: "Magic 8 Ball",
+    imgUrl: "https://proxy.warpcast.com/?url=https%3A%2F%2Fdweb.mypinata.cloud%2Fipfs%2Fbafybeidelrv4tp46l4mn5yzwnzdjtb3p2vyjhqu7mei73vetwtcdkhn4ja&s=7cb5758d9d6c353399548561217d33bfc80150179f7f74c73258116738cd7d34",
+    castUrl: "https://warpcast.com/polluterofminds/0x786f8f4e",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/magic-8-ball"
-  }, 
+  },
   {
-    title: "Mile High Frame Club", 
-    imgUrl: "https://azure-tiny-tahr-350.mypinata.cloud/ipfs/QmUUymhRMCkkNCWhCpsE2VtqitLFn4WJER1xDsLE7ayRcL", 
-    castUrl: "https://warpcast.com/polluterofminds/0x0c685ecd", 
+    title: "Mile High Frame Club",
+    imgUrl: "https://azure-tiny-tahr-350.mypinata.cloud/ipfs/QmUUymhRMCkkNCWhCpsE2VtqitLFn4WJER1xDsLE7ayRcL",
+    castUrl: "https://warpcast.com/polluterofminds/0x0c685ecd",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/mile-high-frame"
-  }, 
+  },
   {
-    title: "Pinata Hub Announcement", 
-    imgUrl: "https://azure-tiny-tahr-350.mypinata.cloud/ipfs/QmWbcF2CPpHbLoXMnarUMRpNu1dvqMS3b4MQG3wDPAnHHh", 
-    castUrl: "https://warpcast.com/polluterofminds/0xc3057c44", 
+    title: "Pinata Hub Announcement",
+    imgUrl: "https://azure-tiny-tahr-350.mypinata.cloud/ipfs/QmWbcF2CPpHbLoXMnarUMRpNu1dvqMS3b4MQG3wDPAnHHh",
+    castUrl: "https://warpcast.com/polluterofminds/0xc3057c44",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/pinata-hub"
-  }, 
+  },
   {
-    title: "Mint a Steve", 
-    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmcBRbicZN7ptxhRxQhf5g7FnBcVEbg8UaPL4pUWfk8Tq9/8.png", 
-    castUrl: "https://warpcast.com/stevedylandev.eth/0xfea11af8", 
+    title: "Mint a Steve",
+    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmcBRbicZN7ptxhRxQhf5g7FnBcVEbg8UaPL4pUWfk8Tq9/8.png",
+    castUrl: "https://warpcast.com/stevedylandev.eth/0xfea11af8",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/steve-frames"
-  }, 
+  },
   {
-    title: "The Weekend", 
-    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmTidtsgh4faygkV3Fj1f2gfdLNYCFh3gsgNVWRsGwSQbA", 
-    castUrl: "https://warpcast.com/stevedylandev.eth/0xe0227b1c", 
+    title: "The Weekend",
+    imgUrl: "https://dweb.mypinata.cloud/ipfs/QmTidtsgh4faygkV3Fj1f2gfdLNYCFh3gsgNVWRsGwSQbA",
+    castUrl: "https://warpcast.com/stevedylandev.eth/0xe0227b1c",
     githubUrl: "https://github.com/PinataCloud/pinataframes/tree/main/src/pages/api/the-weekend"
-  }, 
+  },
   {
-    title: "T3 Emoji", 
-    imgUrl: "https://mktg.mypinata.cloud/ipfs/QmVqEKexfVFiLB3xPXcwDyyUETLowQwvP2rqjhgpet95Cf?filename=emojichat.png", 
-    castUrl: "https://warpcast.com/stevedylandev.eth/0x46e3fee2", 
+    title: "T3 Emoji",
+    imgUrl: "https://mktg.mypinata.cloud/ipfs/QmVqEKexfVFiLB3xPXcwDyyUETLowQwvP2rqjhgpet95Cf?filename=emojichat.png",
+    castUrl: "https://warpcast.com/stevedylandev.eth/0x46e3fee2",
     githubUrl: "https://github.com/PinataCloud/pinataframes/blob/main/src/pages/api/t3.ts"
   },
 ]
@@ -73,7 +79,19 @@ export default function Home() {
   return (
     <div className="w-3/4 m-auto mt-20 mb-20">
       <h1 className="text-center text-4xl mb-8 font-sans text-white font-bold">Pinata Frames & Resources</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 mb-4">
+        <a href="" className="w-full flex items-center justify-center rounded rounded-md bg-white h-20 w-auto shadow shadow-md p-0 m-0">
+          <h2 className="text-center font-bold font-sans text-2xl text-black">Free Farcaster Hub</h2>
+        </a>
+        <a href="" className="w-full flex items-center justify-center rounded rounded-md bg-white h-20 w-auto  text-gray-900 shadow shadow-md p-0 m-0">
+          <h2 className="text-center font-bold font-sans text-2xl text-black">Frame Development Kit</h2>
+        </a>
+        <a href="" className="w-full flex items-center justify-center rounded rounded-md bg-white h-20 w-auto text-gray-900 shadow shadow-md p-0 m-0">
+          <h2 className="text-center font-bold font-sans text-2xl text-black">Frame Analytics</h2>
+        </a>
+      </div>
+      <hr />
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-3 gap-2">
         {
           cards.map((c: Card) => {
             return (
